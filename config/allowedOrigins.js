de3f7cbc508382229a2ae5406030500a44b57607
michelle379 <56913621+michelle379@users.noreply.github.com>
2022-12-08 07:20:24 +0800

    first_commit

---
+++ config/allowedOrigins.js
@@ -0,0 +1,22 @@
+const allowedOrigins = [
+    'http://127.0.0.1:5000',
+    'http://localhost:3500',
+    'http://localhost:3000',
+    'http://localhost:3000/Login',
+    'https://longing-head-production.up.railway.app/Login',
+    'https://longing-head-production.up.railway.app',
+    'https://reactclient-production.up.railway.app',
+    'https://reactclient-production.up.railway.app/',
+    'https://reactclient-production.up.railway.app/Article/',
+    'https://reactclient-production.up.railway.app/Article2/',
+    'https://reactclient-production.up.railway.app/Article/1',
+    'https://reactclient-production.up.railway.app/Article/2',
+    'https://reactclient-production.up.railway.app/Article/3',
+    'https://reactclient-production.up.railway.app/Article/4',
+    'https://reactclient-production.up.railway.app/Article2/1',
+    'https://reactclient-production.up.railway.app/Article2/2',
+    'https://reactclient-production.up.railway.app/Article2/3',
+    'https://reactclient-production.up.railway.app/Article2/4',
+];
+
+module.exports = allowedOrigins;
